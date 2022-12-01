dc6ad843c52d417d71475c74ec699e3ee1692ced
Yuka Saito <yksaito02@gmail.com>
2022-12-01 17:23:10 +0900

    feat: manage notes

--- src/encrypted_notes_backend/src/lib.rs
+++ src/encrypted_notes_backend/src/lib.rs
@@ -1,6 +1,7 @@
 use candid::CandidType;
 use ic_cdk::export::Principal;
 use ic_cdk_macros::*;
+use serde::{Deserialize, Serialize};
 use std::collections::btree_map::Entry::*;
 use std::vec;
 use std::{cell::RefCell, collections::BTreeMap};
@@ -101,6 +102,83 @@ fn delete_device(caller: Principal, device_alias: DeviceAlias) {
     });
 }
 
+#[query(name = "getNotes")]
+fn get_notes(caller: Principal) -> Vec<EncryptedNote> {
+    // TODO ユーザーが登録されているかチェック
+    NOTE_STORE.with(|note_store| {
+        note_store
+            .borrow()
+            .get(&caller)
+            .cloned()
+            .unwrap_or_default()
+    })
+}
+
+#[update(name = "addNote")]
+fn add_note(caller: Principal, encrypted_text: String) -> u128 {
+    // TODO ユーザーが登録されているかチェック
+
+    // TODO: Stringの文字数をチェック
+
+    let id = ID_STORE.with(|id_ref| {
+        let mut note_id = id_ref.borrow_mut();
+        *note_id += 1;
+        *note_id
+    });
+    println!("id: {}", id);
+
+    NOTE_STORE.with(|note_ref| {
+        let mut writer = note_ref.borrow_mut();
+
+        let user_notes = writer.get_mut(&caller).expect("No user is registered.");
+
+        // TODO: Userが所有するノート数をチェック
+
+        // IDとテキストを登録する
+        user_notes.push(EncryptedNote { id, encrypted_text });
+    });
+
+    id
+}
+
+#[update(name = "updateNote")]
+// fn update_note(caller: Principal, update_note: EncryptedNote) {
+fn update_note(caller: Principal, update_id: u128, update_text: String) {
+    // TODO ユーザーが登録されているか(匿名アカウントではないか)チェック
+
+    // TODO: Stringの文字数をチェック
+
+    NOTE_STORE.with(|note_ref| {
+        let mut writer = note_ref.borrow_mut();
+
+        let notes = writer.get_mut(&caller).expect("No user is registered.");
+
+        if let Some(current_note) = notes
+            .iter_mut()
+            .find(|current_note| current_note.id == update_id)
+        {
+            current_note.encrypted_text = update_text;
+        }
+    })
+}
+
+#[update(name = "deleteNote")]
+fn delete_note(caller: Principal, delete_id: u128) {
+    // TODO ユーザーが登録されているかチェック
+
+    NOTE_STORE.with(|note_ref| {
+        let mut writer = note_ref.borrow_mut();
+
+        // let user_notes = writer.get_mut(&caller).expect("No user is registered.");
+
+        // user_notes.retain(|item| item.id != delete_id);
+        writer
+            .get_mut(&caller)
+            .expect("No user is registered.")
+            .retain(|item| item.id != delete_id);
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -179,5 +257,70 @@ mod tests {
         let _res = register_device(principal, device_info.0.clone(), device_info.1.clone());
 
         delete_device(principal, device_info.0);
+    }
+
+    #[test]
+    fn add_and_delete_note() {
+        let device_info = ("Brave".to_string(), "TEST_KEY".to_string());
+
+        // デバイスを登録する
+        let principal = Principal::from_str(TEST_ACCOUNT_1).unwrap();
+        let res = register_device(principal, device_info.0.clone(), device_info.1.clone());
+        assert!(res);
+
+        // テキスト1を追加する
+        let text_1 = "My first text!".to_string();
+        let id_1 = add_note(principal, text_1);
+
+        // テキスト2を追加する
+        let text_2 = "My second text!".to_string();
+        let _id_2 = add_note(principal, text_2);
+
+        // ノートを取得する
+        let notes = get_notes(principal);
+        assert_eq!(notes.len(), 2);
+        println!("{:?}", notes); // TODO delete
+
+        // テキスト1を削除する
+        delete_note(principal, id_1);
+        // ノートを再取得する
+        let notes = get_notes(principal);
+        assert_eq!(notes.len(), 1);
+        println!("{:?}", notes); // TODO delete
+    }
+
+    #[test]
+    fn add_and_update_note() {
+        let device_info = ("Brave".to_string(), "TEST_KEY".to_string());
+
+        // デバイスを登録する
+        let principal = Principal::from_str(TEST_ACCOUNT_1).unwrap();
+        let res = register_device(principal, device_info.0.clone(), device_info.1.clone());
+        assert!(res);
+
+        // テキストを追加する
+        let text = "My first text!".to_string();
+        let id = add_note(principal, text);
+
+        // ノートを取得する
+        let notes = get_notes(principal);
+        assert_eq!(notes.len(), 1);
+        println!("{:?}", notes); // TODO delete
+
+        // ノートを更新する
+        let update_text = "Update text!".to_string();
+
+        // let update = EncryptedNote {
+        //     id,
+        //     encrypted_text: update_text.clone(),
+        // };
+        // update_note(principal, update);
+        update_note(principal, id, update_text.clone());
+
+        // ノートを再取得する
+        let notes = get_notes(principal);
+        assert_eq!(notes.len(), 1);
+        assert_eq!(notes[0].encrypted_text, update_text);
+        println!("{:?}", notes); // TODO delete
     }
 }
